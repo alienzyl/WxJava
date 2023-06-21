cf04678874305cd1ee3c5eead14622af48a4d1f1
alien_zyl <13524702407@163.com>
2023-06-21 16:19:24 +0800

    :new: 【企业微信】添加获客助手api

---
+++ weixin-java-cp/src/main/java/me/chanjar/weixin/cp/bean/external/acquisition/WxCpCustomerAcquisitionRequest.java
@@ -0,0 +1,36 @@
+package me.chanjar.weixin.cp.bean.external.acquisition;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import me.chanjar.weixin.cp.util.json.WxCpGsonBuilder;
+
+/**
+ * @author alien_zyl
+ */
+@Data
+public class WxCpCustomerAcquisitionRequest {
+
+  /**
+   * 获客链接的id
+   */
+  @SerializedName("link_id")
+  private String linkId;
+  /**
+   * 链接名称
+   */
+  @SerializedName("link_name")
+  private String linkName;
+
+  @SerializedName("range")
+  private WxCpCustomerAcquisitionInfo.Range range;
+
+  /**
+   * 是否无需验证，默认为true
+   */
+  @SerializedName("skip_verify")
+  private Boolean skipVerify;
+
+  public String toJson() {
+    return WxCpGsonBuilder.create().toJson(this);
+  }
+}
